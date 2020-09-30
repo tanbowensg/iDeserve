f80e92a6eee6c06b4c3e4405577573b8a2dd4330
谈博文 <tanbowensg@gmail.com>
2020-09-30 10:25:38 +0800

    Initial Commit

---
+++ iDeserve/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDeserve
+//
+//  Created by 谈博文 on 2020/9/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDeserve/iDeserveApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDeserveApp.swift
+//  iDeserve
+//
+//  Created by 谈博文 on 2020/9/30.
+//
+
+import SwiftUI
+
+@main
+struct iDeserveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
