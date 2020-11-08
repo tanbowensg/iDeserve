52f0418c7c4f9569b432b635984ef47b3b65c24a
谈博文 <tanbowensg@gmail.com>
2020-11-08 16:16:50 +0800

    完成points的持久化存储

--- iDeserve/Store/PointsStore.swift
+++ iDeserve/Store/PointsStore.swift
@@ -9,18 +9,34 @@ import Foundation
 
 final class PointsStore: ObservableObject {
     static var shared = PointsStore()
+    
+    private let forKey = "points"
 
-    @Published var points: Int = 100
+    @Published var points: Int = 0
+    
+    init () {
+        let defaults = UserDefaults.standard
+        let _points = defaults.integer(forKey: forKey)
+        self.points = _points
+        
+    }
+    
+    func saveUserDefaults () {
+        let defaults = UserDefaults.standard
+        defaults.set(self.points, forKey: forKey)
+    }
 
     func minus (
         _ value: Int
     ) {
         self.points = self.points - value
+        self.saveUserDefaults()
     }
 
     func add (
         _ value: Int
     ) {
         self.points = self.points + value
+        self.saveUserDefaults()
     }
 }
