01943ed79956f54614bf9f7bbce9572486ff75c1
谈博文 <tanbowensg@gmail.com>
2021-03-17 06:37:37 +0800

    根据一天起始时间，完善刷新重复任务的逻辑

--- iDeserve/Tasks/resetRepeatTaskStatus.swift
+++ iDeserve/Tasks/resetRepeatTaskStatus.swift
@@ -37,14 +37,39 @@ func resetRepeatTaskStatus () {
 
 //检查该任务的完成状态是否需要重置
 func checkTaskStatusShouldReset(_ task: Task) -> Bool {
+    let defaults = UserDefaults.standard
+    let startOfDay = defaults.integer(forKey: START_TIME_OF_DAY)
+    print("一天开始的时间：\(startOfDay)")
+
     if (task.done
         && task.repeatFrequency != RepeatFrequency.never.rawValue
         && task.completeTimes < task.repeatTimes
     ) {
-        let dateDiff = Calendar.current.dateComponents([.day], from: task.lastCompleteTime!, to: Date())
-        print(dateDiff)
+        print("上次完成时间\(task.lastCompleteTime!)")
+        print("当前时间\(Date())")
         let frequency = RepeatFrequency(rawValue: Int(task.repeatFrequency))!
-        return dateDiff.day! >= frequencyDayMap[frequency]!
+        let lastCompleteHour = Calendar.current.component(.hour, from: task.lastCompleteTime!)
+        var intervalDays = frequencyDayMap[frequency]!
+        if lastCompleteHour < startOfDay {
+            intervalDays -= 1
+        }
+        print("应该间隔日:\(intervalDays)")
+//        let dayDiff = Calendar.current.dateComponents([.day], from: task.lastCompleteTime!, to: Date())
+//        print("实际间隔日:\(dayDiff.day)")
+//        if dayDiff.day! > intervalDays {
+//            return true
+//        } else if dayDiff.day == intervalDays {
+//            let currentHour = Calendar.current.component(.hour, from: Date())
+//            print("当前几点:\(currentHour)")
+//            return currentHour >= startOfDay
+//        }
+        var due = Calendar.current.date(byAdding: .day, value: intervalDays, to:  task.lastCompleteTime!)!
+        due = Calendar.current.date(bySettingHour: startOfDay, minute: 0, second: 0, of: due)!
+//        let modifiedDate = Calendar.current.date(byAdding: .hour, value: -startOfDay, to: task.lastCompleteTime!)!
+//        let dateDiff = Calendar.current.dateComponents([.day], from: modifiedDate, to: Date())
+        print("当前时间\(Date())")
+        print("该刷新的时间\(due)")
+        return Date() >= due
     }
     return false
 }
