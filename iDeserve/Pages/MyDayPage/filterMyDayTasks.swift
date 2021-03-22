a2097ffd4ec839c07e3a21e2cead4a47aff88e39
谈博文 <tanbowensg@gmail.com>
2021-03-22 17:37:00 +0800

    我的一天里不显示已经完成的目标的任务

--- iDeserve/Pages/MyDayPage/filterMyDayTasks.swift
+++ iDeserve/Pages/MyDayPage/filterMyDayTasks.swift
@@ -11,14 +11,18 @@ import SwiftUI
 let ddlThreshold = 7 * 24 * 3600
 
 // 过滤出需要显示在我的一天中的任务
+// 目标已经完成的任务不显示
 // 1. 重复到今天的
 // 2. 一周内快要截止的
 // 3. 用户自己设置的
 func filterMyDayTask (_ tasks: FetchedResults<Task>) -> [Task] {
     return tasks.filter({(task: Task) in
-        return task.starred
-            || task.repeatFrequency != RepeatFrequency.never.rawValue
-            || (task.ddl != nil && Int(task.ddl!.timeIntervalSinceNow) < ddlThreshold)
+        return task.parent?.done == false
+            && ( task.starred
+                || task.repeatFrequency != RepeatFrequency.never.rawValue
+                || (task.ddl != nil && Int(task.ddl!.timeIntervalSinceNow) < ddlThreshold
+            )
+        )
     })
 }
 
